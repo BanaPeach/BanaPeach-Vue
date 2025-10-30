04aeb68abae74e56f3d093c30267e40eb6c83d2b
闫世杰 <shijieyan24@gmail.com>
2025-10-31 00:40:47 +0800

    feat(home | project): 集成Unocss + VueBits + ElementPlus并且完成首页导航条的制作

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
-<script setup lang="ts">
-  import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="relative p-0 w-screen h-screen overflow-hidden demo-container bg-black">
+    <PixelBlast
+      :variant="variant"
+      :pixel-size="pixelSize"
+      :color="color"
+      :pattern-scale="patternScale"
+      :pattern-density="patternDensity"
+      :pixel-size-jitter="pixelSizeJitter"
+      :enable-ripples="enableRipples"
+      :liquid="liquid"
+      :speed="speed"
+      :edge-fade="edgeFade"
+    />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- 绝对定位到背景上面 -->
+  <div class="absolute top-0 left-0 w-full h-full pointer-events-none">
+    <Home />
+  </div>
 </template>
 
-<style scoped>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-  }
-</style>
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import PixelBlast, {
+    type PixelBlastVariant
+  } from '@/components/vue-bits/backgrounds/PixelBlast/PixelBlast.vue'
+  import Home from '@/views/home/Home.vue'
+
+  const variant = ref<PixelBlastVariant>('square')
+  const pixelSize = ref(4)
+  const patternScale = ref(2)
+  const patternDensity = ref(1)
+  const pixelSizeJitter = ref(0)
+  const enableRipples = ref(true)
+  const liquid = ref(false)
+  const speed = ref(0.5)
+  const edgeFade = ref(0.25)
+  const color = ref('#A7EF9E')
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 路径别名
 import path from "node:path";
+// Unocss
+import UnoCSS from 'unocss/vite'
+// Element-Plus 自动导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    UnoCSS(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // @ 指向 src 目录
